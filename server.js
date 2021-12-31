fab762c35b19500ac9e6c894538ab5f72ac44209
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-12-31 08:28:59 +0000

    added time stamp w/ date
    added a timeSent value to all messages sent by a user containing the date and time (formatted to English standard - dd/mm/yyyy) the message was sent. this is quite over the top in terms of how many lines it took but I couldn't see a better (at least better + readable) way of doing it.

--- server.js
+++ server.js
@@ -18,24 +18,52 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages/search", (req, res) => {
+  const filteredSearch = messages.filter((message) => {
+    if (message.text.includes(req.query.term)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  res.json(filteredSearch);
 });
 
 app.get("/messages/searchID", (req, res) => {
   res.json(messages.find((message) => message.id == req.query.id));
 });
 
+app.delete("/messages/searchID", (req, res) => {
+  messages = messages.filter((message) => message.id !== req.query.id);
+});
+
 app.post("/messages", (req, res) => {
   const validated = req.body.from != "" && req.body.text != "";
   if (validated) {
     ++currentMessageID;
+    //setup date and time:
+    //date:
+    const d = new Date();
+    const currentDay = d.getDate();
+    const currentMonth = d.getMonth() + 1; // has to be +1 due to
+    const currentYear = d.getFullYear(); // the months starting at 0 instead of 1
+    //time:
+    const hour = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
+    const minute = d.getMinutes();
+    const storedTimeSent = `${currentDay}/${currentMonth}/${currentYear}/${hour}:${minute}`;
+
+    //modify array:
     messages.push({
       id: currentMessageID,
       from: req.body.from,
       text: req.body.text,
+      timeSent: storedTimeSent,
     });
   } else {
     res.status(400).send("invalid field");
